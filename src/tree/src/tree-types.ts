69b9e278dca4fd1cfbfa15d328d6c5207f7fffc0
贝塔棋 <3188864257@qq.com>
2022-07-31 17:25:40 +0800

    feat: :triangular_flag_on_post: tree目录创建

---
+++ src/tree/src/tree-types.ts
@@ -0,0 +1,1 @@
+import { ExtractPropTypes, PropType } from 'vue'
---
+++ src/tree/src/tree.tsx
@@ -0,0 +1,11 @@
+import { defineComponent } from 'vue'
+import { $ref, $ } from 'vue/macros'
+
+export default defineComponent({
+  name: 'STree',
+  setup(props, { slots, emit }) {
+    return () => {
+      return <></>
+    }
+  }
+})
